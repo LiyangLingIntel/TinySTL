3367f0bca1067ba2173af8318c9a05b9d868a354
LiyangLingIntel <liyang.ling@intel.com>
2020-09-01 01:33:41 +0800

    [ADD] Add remove test
    Signed-off-by: LiyangLingIntel <liyang.ling@intel.com>

--- tests/test_insert.cpp
+++ tests/test_insert.cpp
@@ -10,13 +10,34 @@ class RBTreeInsertTest : public ::testing::Test {
 
 };
 
+class RBTreeRemoveTest : public ::testing::Test {
 
-// sample of insertion
+};
 
-TEST_F(/*test suit name*/ RBTreeInsertTest, /*test case name*/ InsertNullTree
-) {
-    RBTree rbtree({5, 7, 8, 3, 2, 6, 4});
+
+
+TEST_F(/*test suit name*/ RBTreeInsertTest, /*test case name*/ InsertTree) {
+    RBTree rbtree({5, 7, 8, 6, 3, 2, 36, 4, 17});
     rbtree.print();
-// ASSERT_EQ(6, insert(36));
-// ASSERT_EQ(true, find(76));
+    ASSERT_NE(rbtree.find(36), nullptr);
+    ASSERT_NE(rbtree.find(6), nullptr);
+    ASSERT_NE(rbtree.find(17), nullptr);
+}
+
+TEST_F(/*test suit name*/ RBTreeInsertTest, /*test case name*/ InsertNullTree) {
+    RBTree rbtree;
+    rbtree.print();
+    ASSERT_EQ(rbtree.find(36), nullptr);
+    ASSERT_EQ(rbtree.find(6), nullptr);
+    ASSERT_EQ(rbtree.find(17), nullptr);
+}
+
+TEST_F(/*test suit name*/ RBTreeRemoveTest, /*test case name*/ RemoveNode) {
+    RBTree rbtree({5, 7, 8, 6, 3, 2, 36, 4, 17});
+    rbtree.print();
+    ASSERT_EQ(rbtree.remove(36), true);
+    ASSERT_EQ(rbtree.remove(2), true);
+    ASSERT_EQ(rbtree.remove(5), true);
+    ASSERT_EQ(rbtree.remove(999), false);
+    rbtree.print();
 }
